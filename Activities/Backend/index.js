326e5f2126ab17d0c9678c96b222c4109a5140c5
CIITSHSDTP00\CIIT <janvincents18@gmail.com>
2026-01-14 13:32:33 +0800

    week 2 upadated backend

--- Activities/Backend/index.js
+++ Activities/Backend/index.js
@@ -3,6 +3,32 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+//get
+
+app.get("/getName", (req, res) => {
+  var name = "Sison";
+  res.status(200).json(name);
+});
+
+//post
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (username == "Sison" && password == "Pass123") {
+    res.status(200).json({
+      message: "login succesful.",
+      status: "successful",
+    });
+  } else {
+    res.status(403).json({
+      message: "invalid username or password",
+      status: "failed",
+    });
+  }
+});
+
 app.listen(PORT, () => {
   console.log("server is running on PORT: ${PORT}");
 });
