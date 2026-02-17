7bbffafc404a2e0577b57b90ba7a33d532e1d73d
Padorusaur <janvincents18@gmail.com>
2026-02-18 00:40:39 +0800

    Week 6

--- Activities/Backend/index.js
+++ Activities/Backend/index.js
@@ -1,4 +1,5 @@
 import experess from "express";
+import inventoryRoutes from "./routes/inventoryRoutes.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
@@ -6,11 +7,17 @@ import authRoutes from "./routes/authRoutes.js";
 const app = experess();
 const PORT = 3000;
 dotenv.config();
-
 // get -> display name, var name = "Your Name"
 // post -> logic, if username="admin" and password="admin" -> success else failed
 
 app.use(experess.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET,POST"],
+  }),
+);
+app.use("/api/inventory", inventoryRoutes);
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
