12daa8b031ac7120694c36b0abbcb65c90a5a741
Padorusaur <janvincents18@gmail.com>
2026-02-18 00:50:09 +0800

    Update login.jsx

--- Activities/frontend/src/pages/login.jsx
+++ Activities/frontend/src/pages/login.jsx
@@ -1,40 +1,63 @@
 import { useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
 import Card from "../components/Card";
 import Input from "../components/Input";
 import Button from "../components/Button";
 import "../styles/Login.css";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
   const [errors, setErrors] = useState({});
   const [loading, setLoading] = useState();
+  const { login } = useAuth();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev_) => ({ ...prev_, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login(formData);
+      alert("Login Successful");
+    } catch (error) {
+      setErrors({ error: error.message });
+    }
+  };
   return (
     <Card title="Welcome Back">
-      <form className="login-form">
+      <form onSubmit={handleSubmit} className="login-form">
         <Input
           label="Email"
           type="email"
           name="email"
+          value={formData.email}
+          onChange={handleChange}
           error={errors.email}
-          placeholder="Enter your email"
+          placeholder="Enter your Email"
           required
         />
         <Input
           label="Password"
           type="password"
           name="password"
+          value={formData.password}
+          onChange={handleChange}
           error={errors.password}
-          placeholder="Enter your password"
+          placeholder="Enter your Password"
           required
         />
         <Button type="submit" loading={loading}>
           Login
         </Button>
-        <p className="auth-link">
-          Don't have an account? <a href="/register">Register here</a>
-        </p>
+        <p className="äuth-link"> Don't have an account? Register here</p>
       </form>
     </Card>
   );
 };
-
 export default Login;
