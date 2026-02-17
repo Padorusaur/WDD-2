3b2cef1a7b2bc64155e7b222750d4e607ec781ce
Padorusaur <janvincents18@gmail.com>
2026-02-18 00:38:08 +0800

    Week 6

--- Activities/Backend/models/Product.js
+++ Activities/Backend/models/Product.js
@@ -1,1 +1,31 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+    }, // e.g., “lucky-me-pancit-canton”
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    category: {
+      type: String,
+      enum: [], //add your categories
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Product", productSchema);
